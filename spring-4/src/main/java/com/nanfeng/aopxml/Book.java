05a88c73b10724076bdd18a3498249cd94eca900
yangnanfeng <y-n-f@foxmail.com>
2023-01-12 22:31:03 +0800

    fix:xml配置aop

---
+++ spring-4/src/main/java/com/nanfeng/aopxml/Book.java
@@ -0,0 +1,7 @@
+package com.nanfeng.aopxml;
+
+public class Book {
+   public void buy() {
+        System.out.println("buy");
+    }
+}
---
+++ spring-4/src/main/java/com/nanfeng/aopxml/BookProxy.java
@@ -0,0 +1,11 @@
+package com.nanfeng.aopxml;
+
+import org.aspectj.lang.annotation.Before;
+
+public class BookProxy {
+
+    public void before() {
+        System.out.println("before-----");
+
+    }
+}
